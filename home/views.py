7c12d5098352c4a053baf2cea4b2c4eb04ea2287
Saurav <sp.gharti@gmail.com>
2018-03-09 17:56:19 +0000

    add package aa_stripe

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'aa_stripe', 'url': 'http://pypi.python.org/pypi/aa_stripe/0.4.1'},
 	{'name':'199Fix', 'url': 'http://pypi.python.org/pypi/199Fix/1.1.2'},
     ]
     context = {
